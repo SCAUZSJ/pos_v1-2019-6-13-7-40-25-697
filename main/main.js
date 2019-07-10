b31ebd51f7720b57e2cdc7bfe85205e395fe8b17
MILO ZHANG (ITA-ISDC-ISD-OOCLL/ZHA) <ZHANGMI6@OOCL.COM>
2019-07-10 14:55:24 +0800

    submit

--- main/main.js
+++ main/main.js
@@ -1,3 +1,32 @@
 'use strict';
 
-//TODO: 请在该文件中实现练习要求并删除此注释
+function printReceipt(inputs) {
+    let obj = objectConversionforMergeRepeat(inputs);
+    console.log(createReceipt(obj));
+  }
+  //用于合并的对象转换
+  function objectConversionforMergeRepeat(inputs){
+    let obj = {};
+    inputs.forEach(item => {
+      let barcode =item.indexOf("-") === -1? item :item.split("-")[0];
+      let addNum = item.indexOf("-") === -1? 1 :item.split("-")[1];
+      obj[item] = item in obj ? ++obj[item] : 1;
+    });
+    return obj;
+  }
+  function createReceipt(obj){
+    let output = `***<没钱赚商店>收据***\n`;
+    let items = loadAllItems();
+    let total = 0;
+    for (var pro in obj) {
+      for (let i = 0; i < items.length; ++i) {
+        if (items[i].barcode == pro) {
+          total += obj[pro] * items[i].price;
+          output += `名称：${items[i].name}，数量：${obj[pro]}${items[i].unit}，单价：${items[i].price.toFixed(2)}(元)，小计：${(obj[pro] * items[i].price).toFixed(2)}(元)\n`
+          break;
+        }
+      }
+    }
+    output += `----------------------\n总计：${total.toFixed(2)}(元)\n**********************`
+    return output;
+  }
