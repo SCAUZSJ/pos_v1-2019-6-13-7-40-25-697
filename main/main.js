39671cb1c02de6d2b26f3e3f90dbd4628595a324
SCAUZSJ <895104160@qq.com>
2019-07-15 20:20:52 +0800

    [Milo]refactor:modify the name of variate and sign the scheduled time.

--- main/main.js
+++ main/main.js
@@ -1,10 +1,10 @@
 'use strict';
-
+//预计3分钟，5分钟
 function printReceipt(inputs) {
   let obj = objectConversionforMergeRepeat(inputs);
   console.log(createReceipt(obj));
 }
-//用于合并的对象转换
+//用于合并的对象转换  预计5分钟，7分钟
 function objectConversionforMergeRepeat(inputs) {
   let obj = {};
   inputs.forEach(item => {
@@ -17,24 +17,25 @@ function objectConversionforMergeRepeat(inputs) {
   });
   return obj;
 }
+//预计5分钟，10分钟
 function createReceipt(obj) {
-  let output = `***<没钱赚商店>收据***\n`;
+  let receipt = `***<没钱赚商店>收据***\n`;
   let items = loadAllItems();
   let total = 0;
-  let free = 0;
+  let discount = 0;
   for (var pro in obj) {
     let item = items.find((val) => {
-      return pro == val.barcode
+      return pro === val.barcode
     })
     total += obj[pro] * item.price;
     let canPromotio = isPromotion(pro, obj[pro]) ;
-    free += canPromotio ? item.price : 0;
-    output += `名称：${item.name}，数量：${obj[pro]}${item.unit}，单价：${item.price.toFixed(2)}(元)，小计：${((obj[pro] * item.price)-(canPromotio ?item.price:0)).toFixed(2)}(元)\n`
+    discount += canPromotio ? item.price : 0;
+    receipt += `名称：${item.name}，数量：${obj[pro]}${item.unit}，单价：${item.price.toFixed(2)}(元)，小计：${((obj[pro] * item.price)-(canPromotio ?item.price:0)).toFixed(2)}(元)\n`
   }
-  output += `----------------------\n总计：${(total-free).toFixed(2)}(元)\n节省：${free.toFixed(2)}(元)\n**********************`
-  return output;
+  receipt += `----------------------\n总计：${(total-discount).toFixed(2)}(元)\n节省：${discount.toFixed(2)}(元)\n**********************`
+  return receipt;
 }
-
+//预计3分钟 实际2分钟
 function isPromotion(barcode, count) {
   let promotions = loadPromotions();
   return promotions[0].barcodes.includes(barcode) && count >= 3;
